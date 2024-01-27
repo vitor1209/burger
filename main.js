644328359aa0fd70c83d32cb4e760ebcaf09f3a7
vitor <137239415+vitor1209@users.noreply.github.com>
2024-01-27 18:46:35 -0300

    Add files via upload
    resposivo pag contato

--- main.js
+++ main.js
@@ -15,3 +15,34 @@ ocultar.forEach((Element) => observador.observe(Element))
 
 // btn menu para celular
 
+function aparecer(){
+  let Menu = document.querySelector('.menu');
+  let btnx = document.querySelector(".btn-menu-x")
+  let btn1 = document.querySelector(".btn-menu")
+  let corpo = document.querySelector(".conteiner")
+
+  if (Menu.classList.contains('open')) {
+
+      Menu.classList.remove('open');
+      btn1.classList.remove('fechar');
+      btnx.classList.remove('abrir');
+      corpo.classList.remove('remover');
+      remov.classList.remove('remover')
+  } 
+  else{
+      Menu.classList.add('open');
+      btn1.classList.add('fechar');
+      btnx.classList.add('abrir');
+      corpo.classList.add('remover');
+      remov.classList.add('remover')
+  }
+  //fixar tela//
+  document.body.classList.toggle('fixo') 
+
+}
+const btn1 = document.querySelector(".btn-menu")
+const menu_mobile = document.querySelector(".menu_mobile")
+
+btn1.onclick = function (){
+  menu_mobile.classList.toggle(".abrir")
+}
